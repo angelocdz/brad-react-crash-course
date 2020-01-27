9f1ec113fc8fe8e58801d5516cd5118e2769d67c
angelocabanerodedicatoria93 <angelodedicatoria93@gmail.com>
2020-01-27 15:51:34 +0800

    added proptypes

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,35 +1,18 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
 class Todos extends Component {
-  /*
-  render() {
-    // console.log(this.props.todos);
-
-    return (
-      <div>
-        <h1>Todos</h1>
-      </div>
-    );
-  }
-  */
-
-  /*
-  render() {
-    return this.props.todos.map((todo) => (
-      // <h3>{1 + 1}</h3>
-      // <h3>{todo.title}</h3>
-      // <TodoItem />
-      <TodoItem todo={todo} />
-    ));
-  }
-  */
-
   render() {
     return this.props.todos.map((todo) => (
       <TodoItem key={todo.id} todo={todo} />
     ));
   }
+}
+
+// PropTypes
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
 }
 
 export default Todos;
